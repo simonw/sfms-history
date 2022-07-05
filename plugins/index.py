d6b60ee44fc5506bfbf78dc7379f05aa6ae62b9b
Simon Willison <swillison@gmail.com>
2022-07-04 17:11:44 -0700

    Implemented keyset pagination, closes #6
    Also styled link and visited link colors.

--- plugins/index.py
+++ plugins/index.py
@@ -15,29 +15,54 @@ order by
     pages_fts.rank, pages.rowid
 """.strip()
 
+
 @hookimpl
 def extra_template_vars(request, template, datasette):
     async def inner():
         db = datasette.get_database()
         if template == "pages/page/{rowid}.html":
             rowid = request.path.split("/")[-1]
-            page = (await db.execute('select * from pages where rowid = ?', (rowid,))).first()
+            page = (
+                await db.execute("select * from pages where rowid = ?", (rowid,))
+            ).first()
             return {
                 "page": page,
             }
         if template == "index.html":
             q = request.args.get("q", "").strip()
+            next = request.args.get("next", "").strip()
             results = []
             count = 0
+            next_token = None
+            not_first_page = False
             if q:
-                search_sql = SEARCH_SQL + " limit 20"
+                kwargs = {"q": q}
+                if next and next.count(":") == 1:
+                    not_first_page = True
+                    search_sql = "with outer as ({}) select * from outer where (rank, rowid) > (cast(:rank as float), cast(:rowid as integer)) limit 21".format(
+                        SEARCH_SQL
+                    )
+                    kwargs["rank"], kwargs["rowid"] = next.split(":")
+                else:
+                    search_sql = SEARCH_SQL + " limit 21"
                 count_sql = "select count(*) from ({})".format(SEARCH_SQL)
                 count = (await db.execute(count_sql, {"q": q})).single_value()
-                results = [dict(r) for r in (await db.execute(search_sql, {"q": q}))]
+                results = [
+                    dict(r, filename=r["path"].split("/")[-1])
+                    for r in (await db.execute(search_sql, kwargs))
+                ]
+                if len(results) > 20:
+                    results = results[:20]
+                    next_token = "{}:{}".format(
+                        results[-1]["rank"], results[-1]["rowid"]
+                    )
             return {
                 "q": q,
                 "results": results,
                 "count": count,
+                "next_token": next_token,
+                "not_first_page": not_first_page,
             }
         return {}
+
     return inner

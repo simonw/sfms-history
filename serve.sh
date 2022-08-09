b38cfd132627e84bae9c8cbd928b47bffc1ede64
Simon Willison <swillison@gmail.com>
2022-08-09 10:10:55 -0700

    serve.sh no longer confused by extra .db files

--- serve.sh
+++ serve.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 SFMS_PASSWORD_HASH='pbkdf2_sha256$260000$0427d3f9bd2936ca4e7243321242c674$OjCOtKOVa9UPYq0DsUjCastYHrQQlpFdh/0GZfbVteU=' \
-  datasette . -p 8006 --reload
+  datasette sfms.db \
+    -p 8006 \
+    --reload \
+    --plugins-dir plugins \
+    --template-dir templates \
+    --static static:static \
+    -m metadata.yml
